aba11498bd052a9f8b9c280b7a96f70417b3dc99
Komi Wolanyo K0UDO <164168359+komi-eulisa@users.noreply.github.com>
2026-02-26 17:25:45 +0100

    chore: make the app distributable (#38)
    - include: rename the default log config file name
    - include: add a custom banner

---
+++ connector-distribution/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("base")
+}
+
+val mockitoAgent: Configuration = configurations.create("mockitoAgent")
+
+var bootstrapperJar = project(":connector-bootstrapper").tasks.named("bootJar")
+
+val prepareDistribution by tasks.registering(Copy::class) {
+    dependsOn(bootstrapperJar)
+
+    from(bootstrapperJar.map { it.outputs.files }) {
+        include("*.jar")
+        into("bin")
+    }
+    // copy application.properties and log4j2.xml externally
+    from(project(":connector-bootstrapper").file("src/main/resources")) {
+        include("application.properties", "log4j2.xml", "banner.txt")
+        into("config")
+    }
+    from("src/main/resources/bin") {
+        include("start.sh", "start.bat")
+        eachFile {
+            if (name == "start.sh") {
+                filePermissions {
+                    unix("rwxr-xr-x")
+                }
+            }
+        }
+    }
+
+    into(layout.buildDirectory.dir("connector-distribution-${version}"))
+}
+
+// Task: Build a zip distribution
+tasks.register<Zip>("distributionZip") {
+    dependsOn(prepareDistribution)
+    from(layout.buildDirectory.dir("connector-distribution-${version}"))
+    archiveFileName.set("connector-distribution-${version}.zip")
+    destinationDirectory.set(layout.buildDirectory)
+}
+
+tasks.named("assemble") {
+    dependsOn("distributionZip")
+}
---
+++ connector-distribution/src/main/resources/bin/start.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem
+rem Copyright 2026 European Union Agency for the Operational Management of Large-Scale IT Systems
+rem in the Area of Freedom, Security and Justice (eu-LISA)
+rem
+rem Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the
+rem European Commission - subsequent versions of the EUPL (the "Licence");
+rem You may not use this work except in compliance with the Licence.
+rem You may obtain a copy at: https://joinup.ec.europa.eu/software/page/eupl
+rem
+
+java -jar .\bin\connector-ng.jar --spring.config.location=.\config\

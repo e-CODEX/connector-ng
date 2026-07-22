40085de940796645eea4bfd2f985547d51c8051a
Komi Wolanyo K0UDO <164168359+komi-eulisa@users.noreply.github.com>
2026-07-22 14:12:28 +0200

    feat: add business details to msg detail dto (#188)

--- connector-infrastructure/src/main/java/eu/ecodex/connector/infrastructure/inbound/web/rest/dto/message/ConnectorMessageDetailDto.java
+++ connector-infrastructure/src/main/java/eu/ecodex/connector/infrastructure/inbound/web/rest/dto/message/ConnectorMessageDetailDto.java
@@ -15,6 +15,7 @@ import eu.ecodex.connector.domain.model.message.ConnectorMessageAS4Properties;
 import eu.ecodex.connector.domain.model.message.ConnectorMessageDirection;
 import eu.ecodex.connector.domain.model.message.ConnectorMessageError;
 import eu.ecodex.connector.domain.model.message.attachment.ConnectorMessageAttachment;
+import eu.ecodex.connector.domain.model.message.content.ConnectorMessageBusinessContent;
 import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
@@ -35,6 +36,7 @@ import lombok.Builder;
  *                                            message
  * @param gatewayName                         name of the gateway responsible for transmitting or
  *                                            receiving the message
+ * @param businessContent                     business content of the message
  * @param as4Properties                       AS4-specific metadata and properties associated with
  *                                            the message
  * @param direction                           direction of the message flow (e.g., inbound or
@@ -45,10 +47,6 @@ import lombok.Builder;
  * @param deletedAt                           timestamp when the message was deleted
  * @param rejectedAt                          timestamp when the message was rejected
  * @param confirmedAt                         timestamp when the message was confirmed
- * @param deliveredToGatewayAt                timestamp when the message was delivered to the
- *                                            gateway
- * @param deliveredToBackendAt                timestamp when the message was delivered to the
- *                                            backend system
  * @param attachments                         list of attachments associated with the message
  * @param evidences                           list of evidences related to message processing or
  *                                            delivery
@@ -63,6 +61,7 @@ public record ConnectorMessageDetailDto(
     String referenceToBackendMessageIdentifier,
     String backendName,
     String gatewayName,
+    ConnectorMessageBusinessContent businessContent,
     ConnectorMessageAS4Properties as4Properties,
     ConnectorMessageDirection direction,
     boolean isBusiness,
@@ -96,6 +95,7 @@ public record ConnectorMessageDetailDto(
             .isBusiness(message.isBusinessMessage())
             .backendName(message.backendName())
             .gatewayName(message.gatewayName())
+            .businessContent(message.businessContent())
             .as4Properties(message.as4Properties())
             .createdAt(message.createdAt())
             .updatedAt(message.updatedAt())
